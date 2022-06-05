75dbcb96d66286cffa715a4e114592fd4b5e0d1e
vincewu124 <vincewu124@hotmail.com>
2022-06-04 18:00:47 -0700

    weather api backend

--- root/backend/src/routes/weatherAPI.ts
+++ root/backend/src/routes/weatherAPI.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import axios from 'axios';
+
+require('dotenv').config();
 
 const router: express.Router = express.Router();
 
-router.get('/getWeatherInfo', async (req: express.Request, res: express.Response) => {
+router.get('/getWeatherInfo', async (req, res) => {
+  const key: string = process.env.WEATHER_API_KEY!;
+  const url: string = `https://api.weatherapi.com/v1/current.json?key=${key}&q=Vancouver`;
   try {
-    res.send({ test: 'worked' });
+    const weatherInfo = await axios.get(url);
+    res.send(weatherInfo.data);
   } catch (err) {
     res.json({ message: err });
   }
