fb44371248b542c46790a0944a29b87934ead45f
vincewu124 <vincewu124@hotmail.com>
2022-06-28 22:14:20 -0700

    db setup

--- root/backend/src/server.ts
+++ root/backend/src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 //  routes
 import weatherAPI from './routes/weatherAPI';
 
+require('dotenv/config');
+
 const app: express.Application = express();
+const key: string = process.env.WEATHER_API_KEY!;
 
 //  middleware
 app.use(cors());
@@ -12,7 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/weather', weatherAPI);
 
+//  db connection
+mongoose.connect(key, () => {
+  // eslint-disable-next-line no-console
+  console.log('connect to db');
+});
+
 app.listen(5005, () => {
-// eslint-disable-next-line no-console
+  // eslint-disable-next-line no-console
   console.log('server listening on 5005');
 });
