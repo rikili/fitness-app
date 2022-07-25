4e01a1b265e5888f0e2041524422ff108c682d0f
Vincent Wu <vincewu124@hotmail.com>
2022-07-25 01:05:48 -0700

    updated styling

--- root/frontend/src/routes/Home.tsx
+++ root/frontend/src/routes/Home.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 
-import { ThemeProvider } from 'react-jss';
 import Calendar from '../components/Calendar/Calendar';
 import PreviewCard from '../components/PreviewCard/PreviewCard';
 
 function Home() {
-  //  placeholder until we populate database with exercises + theme colors
-  const theme = {
-    color: 'red',
-  };
-
   return (
     <div>
       <Calendar />
       {/* this is a placeholder */}
-      <ThemeProvider theme={theme}>
-        <PreviewCard title="workout" exercises={['arms with long title', 'legs', 'chest', 'back', 'abs', 'shoulders', '7th child (not shown)']} />
-      </ThemeProvider>
+      <PreviewCard title="workout" exercises={['arms with long title', 'legs', 'chest', 'back', 'abs', 'shoulders', '7th child (not shown)']} theme="green" />
+      <PreviewCard title="workout2" exercises={['arms with long title', 'legs', 'chest', 'back', 'abs', 'shoulders', '7th child (not shown)']} theme="blue" />
     </div>
   );
 }
